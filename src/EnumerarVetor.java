8d938f6ed11456faebf578673f752766e97a0444
jpereira <jobsonrp@gmail.com>
2018-01-26 22:41:48 -0300

    Commit Inicial (Antes)

---
+++ src/EnumerarVetor.java
@@ -0,0 +1,25 @@
+import java.util.Vector;
+import java.util.Enumeration;
+ 
+public class EnumerarVetor {
+ 
+  public static void main(String[] args) {
+    //create a Vector object
+    Vector v = new Vector();
+   
+    //populate the Vector
+    v.add("One");
+    v.add("Two");
+    v.add("Three");
+    v.add("Four");
+    v.add("Five");
+   
+    //Get Enumeration of Vector's elements using elements() method
+    Enumeration e = v.elements();
+     
+    System.out.println("Elements of the Vector are : ");
+   
+    while(e.hasMoreElements())
+      System.out.println(e.nextElement());
+  }
+}
